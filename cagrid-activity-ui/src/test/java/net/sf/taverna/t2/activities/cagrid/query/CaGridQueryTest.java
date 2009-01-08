359fdbc5543a940f6837a55e18b5cd8e84b37935
tanw <tanw>
2009-01-08 15:49:22 +0000

    adding junit test; one test is ignored

---
+++ cagrid-activity-ui/src/test/java/net/sf/taverna/t2/activities/cagrid/query/CaGridQueryTest.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (C) 2007-2009 The University of Manchester   
+ * Copyright (C) 2009 The University of Chicago
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package net.sf.taverna.t2.activities.cagrid.query;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import gov.nih.nci.cagrid.cadsr.client.CaDSRServiceClient;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Ignore;
+
+public class CaGridQueryTest {
+
+	private static String indexURL;
+	@BeforeClass
+	public static void setup() {
+		//TODO should read from an external resource
+		indexURL="http://cagrid-index.nci.nih.gov:8080/wsrf/services/DefaultIndexService";
+	}
+	
+	
+	@Ignore("JUnit uses a flat classpath so both Taverna and Globus axis are in classpth, this test will fail")
+	@Test
+	public void testDoQuery() {
+		CaGridQuery q = new CaGridQuery(indexURL,null);
+		q.doQuery();
+		WSDLActivityItem i = (WSDLActivityItem)q.toArray()[0];
+		assertEquals("The type should be WSDL","WSDL",i.getType());
+		assertEquals("The style should be document","document",i.getStyle());
+		assertNotNull("The operation should be set",i.getOperation());
+		assertTrue("The operation should be have some content",i.getOperation().length()>2);
+	}
+	
+}
