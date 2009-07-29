6fc5d12133e79c383bd7d595addc385c522e28eb
stians <stians>
2009-07-29 13:32:34 +0000

    added servicewrapper-service-ncbiblast

---
+++ src/uk/org/mygrid/cagrid/servicewrapper/service/ncbiblast/common/NCBIBlastConstants.java
@@ -0,0 +1,13 @@
+package uk.org.mygrid.cagrid.servicewrapper.service.ncbiblast.common;
+
+import javax.xml.namespace.QName;
+
+
+/**
+ * Constants class that extends the introduce managed constants.  Developers can add constants to this file.
+ *
+ * @created by Introduce Toolkit version 1.3
+ */
+public interface NCBIBlastConstants extends NCBIBlastConstantsBase {
+	
+}
---
+++ src/uk/org/mygrid/cagrid/servicewrapper/service/ncbiblast/job/service/globus/resource/NCBIBlastJobResource.java
@@ -0,0 +1,17 @@
+package uk.org.mygrid.cagrid.servicewrapper.service.ncbiblast.job.service.globus.resource;
+
+import org.globus.wsrf.InvalidResourceKeyException;
+import org.globus.wsrf.NoSuchResourceException;
+import org.globus.wsrf.ResourceException;
+import org.globus.wsrf.ResourceKey;
+
+
+/** 
+ * The implementation of this NCBIBlastJobResource type.
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class NCBIBlastJobResource extends NCBIBlastJobResourceBase {
+
+}
