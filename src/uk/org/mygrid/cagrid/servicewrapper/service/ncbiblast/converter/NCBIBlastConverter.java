202c533f07e3d90f55f09eddecd36ba407f37d33
stians <stians>
2009-08-07 15:50:32 +0000

    expose databases

--- src/uk/org/mygrid/cagrid/servicewrapper/service/ncbiblast/converter/NCBIBlastConverter.java
+++ src/uk/org/mygrid/cagrid/servicewrapper/service/ncbiblast/converter/NCBIBlastConverter.java
@@ -1,19 +1,25 @@
 package uk.org.mygrid.cagrid.servicewrapper.service.ncbiblast.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.xmlbeans.XmlException;
 import org.jdom.Document;
 import org.jdom.JDOMException;
 
+import uk.org.mygrid.cagrid.domain.common.Database;
 import uk.org.mygrid.cagrid.domain.ncbiblast.NCBIBLASTInput;
 import uk.org.mygrid.cagrid.domain.ncbiblast.NCBIBLASTOutput;
 import uk.org.mygrid.cagrid.servicewrapper.serviceinvoker.ncbiblast.NCBIBlastInput;
+import uk.org.mygrid.cagrid.servicewrapper.serviceinvoker.ncbiblast.SequenceDatabase;
 
 public class NCBIBlastConverter {
 	NCBIBlastImporter importer = new NCBIBlastImporter();
-	
+
 	NCBIBlastExporter exporter = new NCBIBlastExporter();
-	
-	public NCBIBLASTOutput convertNCBIBlastOutput(Document data) throws ConverterException {
+
+	public NCBIBLASTOutput convertNCBIBlastOutput(Document data)
+			throws ConverterException {
 		try {
 			return importer.importNCBIBlastOutput(data);
 		} catch (XmlException e) {
@@ -23,10 +29,19 @@ public class NCBIBlastConverter {
 		}
 	}
 
-	public NCBIBlastInput convertNCBIBlastInput(NCBIBLASTInput nCBIBlastInput) throws ConverterException {
+	public NCBIBlastInput convertNCBIBlastInput(NCBIBLASTInput nCBIBlastInput)
+			throws ConverterException {
 		return exporter.exportNCBIBlastInput(nCBIBlastInput);
-		
+
 	}
-	
-	
+
+	public List<Database> convertDatabases(List<SequenceDatabase> sequenceDBs) {
+		List<Database> databases = new ArrayList<Database>();
+		for (SequenceDatabase sequenceDB : sequenceDBs) {
+			Database database = new Database();
+			database.setName(sequenceDB.getName());
+		}
+		return databases;
+	}
+
 }
