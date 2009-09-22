fdf95346ae5da1685b276e6ad220bf6e52ab04fe
tanw <tanw>
2009-09-22 23:37:23 +0000

    cagrid-activity-ui plug-in for Taverna 2.0 with caGrid 1.3 release in Feb 2009

---
+++ t2-cagrid/cagrid-ui/src/main/java/gov/nih/nci/cagrid/workflow/factory/client/TavernaWorkflowServiceClientMain.java
@@ -0,0 +1,142 @@
+package gov.nih.nci.cagrid.workflow.factory.client;
+
+import gov.nih.nci.cagrid.common.Utils;
+
+//import java.io.File;
+//import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.axis.message.addressing.EndpointReferenceType;
+//import org.apache.axis.types.URI.MalformedURIException;
+
+import workflowmanagementfactoryservice.WorkflowOutputType;
+import workflowmanagementfactoryservice.WorkflowStatusType;
+
+public class TavernaWorkflowServiceClientMain {
+	
+	public static void main(String [] args){
+		
+		System.out.println("Running the Grid Service Client");		
+		System.out.println("OS: " + System.getProperty("user.home"));
+
+		Map map = new HashMap();
+		for (int i = 0; i< args.length; i++)
+		{
+			if(args[i].startsWith("-"))
+			{
+				map.put(args[i], args[++i]);						
+			} else {
+				TavernaWorkflowServiceClient.usage();
+				System.exit(1);
+			}
+		}
+
+		try{
+			if(!(args.length < 4)){
+				if(args[0].equals("-url")){
+
+					
+					// 1. Create Workflow Operations invoked.
+
+					if(!map.containsKey("-scuflDoc"))
+					{
+						TavernaWorkflowServiceClient.usage();
+					}
+					String url = (String) map.get("-url");
+					String scuflDoc = (String) map.get("-scuflDoc");
+					TavernaWorkflowServiceClient client = new TavernaWorkflowServiceClient(url);
+
+					String workflowName = "Test1";
+
+
+					System.out.println("\n1. Running createWorkflow ..");
+
+					//WMSOutputType wMSOutputElement =  client.createWorkflow(input);
+					EndpointReferenceType resourceEPR = TavernaWorkflowServiceClient.setupWorkflow(url, scuflDoc, workflowName);
+
+					System.out.println("Created a resource with EPR ..");
+					System.out.println("Writing EPR to file ..");
+					TavernaWorkflowServiceClient.writeEprToFile(resourceEPR, workflowName);
+
+					//************************************************************************// 
+
+					// 2. Start Workflow Operations Invoked.
+					//
+
+					String[] inputArgs = {"Taverna", " and caGrid"}; 
+					
+					//String[] inputArgs = {"caCore"}; 
+					
+					System.out.println(inputArgs.length);
+					
+					System.out.println("\n2. Now starting the workflow ..");
+					System.out.println("Reading EPR from file ..");
+					EndpointReferenceType readEPR = new EndpointReferenceType();
+					try {
+						System.out.println(System.getProperty("user.dir") + "/" + workflowName + ".epr");
+						readEPR = TavernaWorkflowServiceClient.readEprFromFile(System.getProperty("user.dir") + "/"+ workflowName + ".epr");
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+					//This method runs the workflow with the resource represented by the EPR.
+					// If there is no inputFile for the workflow, give "null"
+					WorkflowStatusType workflowStatusElement =  TavernaWorkflowServiceClient.startWorkflow(inputArgs, readEPR);
+					
+					if (workflowStatusElement.equals(WorkflowStatusType.Done))
+					{
+						System.out.println("Workflow successfully executed..");
+					}
+					else
+					{
+						throw new Exception("Failed to execute the workflow! Please try again.");
+					}
+					
+					
+
+					// 3. Get Status operation invoked.
+					System.out.println("\n3. Checking the status of the workflow..");
+					WorkflowStatusType workflowStatus = TavernaWorkflowServiceClient.getStatus(readEPR);
+					if(workflowStatus.equals(WorkflowStatusType.Done))
+					{
+						System.out.println("Workflow Executions is Completed.");
+					}
+					else if (workflowStatus.equals(WorkflowStatusType.Failed))
+					{
+						System.out.println("Workflow failed to execute.");
+					}
+					else if (workflowStatus.equals(WorkflowStatusType.Pending))
+					{
+						System.out.println("Workflow execution is pending.");
+					}
+
+					//4. Get output of workflow.
+
+					System.out.println("\n4. Getting back the output file..");
+					WorkflowOutputType workflowOutput = TavernaWorkflowServiceClient.getOutput(readEPR);
+					
+					String[] outputs = workflowOutput.getOutputFile();
+					//for now the service only returns one output file
+					for (int i=0; i < outputs.length; i++)
+					{
+						String outputFile = System.getProperty("user.dir") + "/Test1-output-" + i + ".xml";
+						Utils.stringBufferToFile(new StringBuffer(outputs[i]), outputFile);
+						System.out.println("Output file " + i + " : " + outputFile);
+					}
+
+				} else {
+					TavernaWorkflowServiceClient.usage();
+					System.exit(1);
+				}
+			} else {
+				TavernaWorkflowServiceClient.usage();
+				System.exit(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
---
+++ t2-cagrid/cagrid-ui/src/main/java/net/sf/taverna/t2/workbench/cagrid/CaGridRun.java
@@ -0,0 +1,62 @@
+/******************************************************************************* 
+ * Copyright (C) 2008 The University of Chicago
+ * @author Wei Tan
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package net.sf.taverna.t2.workbench.cagrid;
+
+//import java.awt.BorderLayout;
+import java.util.Date;
+//import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import javax.swing.JPanel;
+//import javax.swing.JTabbedPane;
+//import javax.swing.JTextArea;
+
+import net.sf.taverna.t2.reference.T2Reference;
+
+
+//classes to invoke the caGrid service to execute a given workflow
+public class CaGridRun {
+	//workflow instance identifier
+	public int workflowid;
+	//workflow inputs
+	private Map<String, T2Reference> inputs;
+	//time for execution
+	private Date date;
+	//xml string that represent the execution results -- may be multiple results
+	public Map<String, String> outputMap;
+	//the container to show multiple results
+	public JPanel resultPanel;
+	public String status;// initiated, executing, completed
+	CaGridRun(){
+		inputs =null;
+		date = new Date();
+		Random generator = new Random();
+		workflowid = generator.nextInt();
+		resultPanel = new JPanel();
+		resultPanel.setName(String.valueOf(workflowid));
+		status = "initiated";
+		outputMap = null;
+		
+	}
+
+}
